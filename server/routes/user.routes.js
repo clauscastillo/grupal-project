180e58829bce8941cb219ad438307b9bec15a981
asbae <asbaez@outlook.com>
2023-03-13 00:19:08 -0300

    cambiar rutas

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,13 +1,23 @@
-const { registerUser, loginUser, getUser, loginUserInternal} = require('../controllers/user.controller')
+const { registerUser, loginUser, register, login, isLogged, logOutUser, getUser, loginUserInternal } = require('../controllers/user.controller')
 
 
 module.exports = (app) => {
-    app.post('/api/registeruser', registerUser)
+    // app.post('/api/registeruser', registerUser)
 
-    app.post('/api/login', loginUser)
+    // app.post('/api/login', loginUser)
 
     app.post('/api/login/internal', loginUserInternal)
 
-    app.get('/api/user', getUser )
+    app.get('/api/user', getUser)
+
+    app.post('/api/registeruser', register)
+
+    app.get('/api/login', login)
+
+    app.get('/api/logout', logOutUser)
+
+    app.get('/api/islogged', isLogged)
+
+
 }
 
