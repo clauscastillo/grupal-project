cb14c885f02f69d7d57be9c240611a1f96f92b3d
asbae <asbaez@outlook.com>
2023-03-16 22:55:02 -0300

    limpieza

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -7,66 +7,6 @@ import toast from 'react-hot-toast';
 const Login = () => {
   const [loading, setLoading] = useState(false);
   const [response, setResponse] = useState(null)
-
-  // const initialForm = {
-  //   email: '',
-  //   password: '',
-  // }
-
-  //   const validationsForm = (form) => {
-  //     let errors = {};
-  //     let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
-  //     const {email, password} = form
-
-  //     if(!regexEmail.test(email)){
-  //       errors.email = 'Complete un email valido'
-  //     }
-  //     if(!email.trim()){
-  //       errors.email = 'Complete su email'
-  //     }
-  //     if(password.length < 8) {
-  //       errors.password = 'La contraseña debe tener 8 caracteres como minimo'
-  //     }
-  //     if(!password.trim()){
-  //       errors.password = 'Complete su contraseña'
-  //     }
-
-
-  //     return errors
-  //   } 
-
-  //   const {form, errors, loading, response, handleChange, handleBlur, handleSubmit, handleOver} = useForm(initialForm, validationsForm, 'login')
-
-  //   return (
-  //     <div className='col px-5'>
-  //       <form className='col w-75' onSubmit={handleSubmit}>
-  //         <h3>Iniciar sesión</h3>
-  //         <div>
-  //           <label className='form-label'>Correo</label>
-  //           <input type="text" name="email" className='form-control' value={form.email} onChange={handleChange} onBlur={handleBlur}/>
-  //           {
-  //             errors.email && <p className='text-danger'>{errors.email}</p>
-  //           }
-  //         </div>
-  //         <div>
-  //           <label className='form-label'>Contraseña</label>
-  //           <input type="password" name="password" className='form-control' value={form.password} onChange={handleChange} onBlur={handleBlur}/>
-  //           {
-  //             errors.password && <p className='text-danger'>{errors.password}</p>
-  //           }
-  //         </div>
-  //         <button className='btn btn-primary' type='submit' onMouseOver={handleOver}>Login</button>  
-  //       </form>
-  //     </div>
-
-  //   )
-  // }
-
-  // export default Login
-
-
-
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState([])
--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useNavigate, Outlet, redirect} from "react-router-dom";
-import { useState, useEffect } from "react";
 import { isLogged, isLogged2 } from "../utils/sessionUtils";
 
 // customHook
@@ -13,29 +12,6 @@ async function useAuth(resultado){
     return redirect("/login")
   })
   return result
-  
-  // const [active, setActive] = useState(false)
-  // useEffect(() => {
-  //   isLogged2(navigate)
-  //   .then((response)=>{
-  //     console.log(response, " KIUBOLE")
-  //     setActive(true) 
-  //     return active
-  //   }).catch((error)=>{
-  //     console.log(error)
-  //   })
-  // },[active])
-  //   return active;
-
-     
-    // isLogged2(navigate)
-    // .then((res)=>{
-    // console.log(res, " KIUBOLE")
-    // return res;
-    // }).catch((err)=>{
-    //   console.log(err)
-    // })
-
 }
 
 const PrivateRoute = ({children, loaded, setLoaded}) => {
@@ -50,42 +26,11 @@ const PrivateRoute = ({children, loaded, setLoaded}) => {
     console.log("welcome")
     return redirect("/login")
    })
-  //   const resultado = isAuth.then((res)=>{return res})
-  //   console.log(resultado, " IS AUTH")
-    
-    
-    // useEffect(() => {
-    //     isLogged()
-    //     .then((res) =>{
-    //         console.log(res, " QUE NOS RETORNA isLogged?")
-    //         setLoaded(true)
-    //         console.log(loaded, "YA SETEO???????")
-    //     }).catch((error)=>{
-    //         console.log(error)
-    //     });
-
-    //   }, []);
-
-
-
-    // useEffect(()=>{
-    //     axios.get('http://localhost:8000/api/isLogged', {withCredentials:true})
-    //     .then((res)=>{
-    //         console.log(res.data.active)
-    //         setLoaded(res.data.active)
-    //     }).catch((err)=>{
-    //         console.log(err)
-    //     })
-    // }, [])
-
-    // setLoaded(true)
-
 
   return (
     <>
     {isAuth ? <Outlet/>:"bajan"}
     {isAuth ? children: "spinetta"}
-    {/* <Outlet/> */}
     </>
   )
 }
